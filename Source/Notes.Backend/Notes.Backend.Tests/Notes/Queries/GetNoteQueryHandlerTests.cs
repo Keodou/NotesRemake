a314ec2fc532a1f5286516a490fa825f432ce0cd
Dmitry Korobeyshchikov <korobeyshchikov.d@mail.ru>
2023-09-10 16:36:24 +0300

    Wrote a test to get details about the note

---
+++ Source/Notes.Backend/Notes.Backend.Tests/Notes/Queries/GetNoteQueryHandlerTests.cs
@@ -0,0 +1,25 @@
+﻿using Notes.Backend.Application.Notes.Queries.GetNote;
+using Notes.Backend.Tests.Common;
+
+namespace Notes.Backend.Tests.Notes.Queries
+{
+    public class GetNoteQueryHandlerTests : TestCommandBase
+    {
+        [Fact]
+        public async Task GetNoteQueryHandler_Success()
+        {
+            // Arrange
+            var handler = new GetNoteQueryHandler(_context);
+
+            // Act
+            var result = await handler.Handle(
+                new GetNoteQuery
+                {
+                    NoteId = NotesContextFactory.NoteIdForUpdate
+                }, CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
